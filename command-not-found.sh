78c75940128e777fc36b95166e68f99fd75cdd20
Matthew Bauer <mjbauer95@gmail.com>
2017-05-13 20:19:52 -0500

    Add check when inside terminal or midnight commander
    This article has tutorial on building a "command-not-found-handler"
    and doing this to prevent unexpected behavior.
    
    http://www.linuxjournal.com/content/bash-command-not-found

--- command-not-found.sh
+++ command-not-found.sh
@@ -4,8 +4,18 @@
 # this will be called when a command is entered
 # but not found in the user’s path + environment
 command_not_found_handle () {
-    toplevel=nixos # TODO: detect this somehow
-    cmd=$1 # TODO: differentiate between paths and commands
+
+    # TODO: use "command not found" gettext translations
+
+    # taken from http://www.linuxjournal.com/content/bash-command-not-found
+    # - do not run when inside Midnight Commander or within a Pipe
+    if [ -n "$MC_SID" ] || ! [ -t 1 ]; then
+        >&2 echo "$1: command not found"
+        return 127
+    fi
+
+    toplevel=nixpkgs # nixpkgs should always be available even in NixOS
+    cmd=$1
     attrs=$(@out@/bin/nix-locate --minimal --no-group --type x --top-level --whole-name --at-root "/bin/$cmd")
     len=$(echo -n "$attrs" | grep -c "^")
 
