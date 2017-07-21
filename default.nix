5f3240f03f81038ec7e54fa8ad261e66646b8e85
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2017-07-21 12:30:50 +0200

    fix(default.nix): update to new dependencies

--- default.nix
+++ default.nix
@@ -1,18 +1,30 @@
 let
-  # nixpkgs-unstable at 2017-06-16 10:24
-  nixpkgsRev = "afd2bdbad25db4b0007b52d07161345e6426ae72";
+  # nixpkgs-unstable at 2017-07-21 12:12
+  nixpkgsRev = "1d78df27294017d464e60bf9b0595e7e42cfca55";
   defaultNixpkgs = builtins.fetchTarball "github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
 in
 { nixpkgs ? defaultNixpkgs }:
 
 with (import nixpkgs {}); with rustPlatform;
 
-buildRustPackage rec {
+let
+  registry = rustRegistry.overrideAttrs (old: {
+    name = "rustRegistry-2017-07-21";
+    src = fetchFromGitHub {
+      owner = "rust-lang";
+      repo = "crates.io-index";
+      rev = "25ee65416ccd75fd0bdbcc31affb119513dd2951";
+      sha256 = "1y2mlj31xpzw5h2l4rl1r74gc58lagqsmcmfj0fjgmjzv7glqcwz";
+    };
+  });
+in buildRustPackage rec {
   name = "nix-index-${version}";
   version = "0.1.0";
 
+  rustRegistry = registry;
+
   src = builtins.filterSource (name: type: !lib.hasPrefix "target" (baseNameOf name) && !lib.hasPrefix "result" (baseNameOf name) && name != ".git") ./.;
-  depsSha256 = "0wynn3jn30wza3608h33x3psb26gz1bcmxc48pdixk0f620vpmjc";
+  depsSha256 = "0v145fi9bfiwvsdy7hz9lw4m2f2j8sxvixfzmjwfnq4klm51c8yl";
   buildInputs = [pkgconfig openssl curl];
 
   postInstall = ''
