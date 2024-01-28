6f88ef16ad9037a364d0add0754386ee399e587a
Valentin Gagarin <valentin.gagarin@tweag.io>
2024-01-28 22:19:06 +0100

    nix-env -> nix-shell
    - don't recommend `nix-env` any more
    - add `shell.nix` to allow running the program with `nix-shell --run`
    - move build instructions to separate files

--- default.nix
+++ default.nix
@@ -1,11 +1,73 @@
-# This file is the compt layer of flakes: https://github.com/edolstra/flake-compat
-# See flake.nix for details
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash; }
-) {
-  src =  ./.;
-}).defaultNix
+{
+  system ? builtins.currentSystem,
+  inputs ? import (fetchTarball "https://github.com/fricklerhandwerk/flake-inputs/tarball/1.0") {
+    root = ./.;
+  },
+  nixpkgs-config ? {
+    inherit system;
+    config = { };
+    overlays = [ ];
+  },
+}:
+let
+  # avoid re-importing `nixpkgs` if it comes from `flake.nix`
+  pkgs =
+    if inputs.nixpkgs ? lib then
+      inputs.nixpkgs.legacyPackages.${system}
+    else
+      import inputs.nixpkgs nixpkgs-config;
+in
+rec {
+  packages = {
+    default = pkgs.callPackage ./package.nix { };
+  };
+  devShells = {
+    minimal =
+      with pkgs;
+      mkShell {
+        name = "nix-index";
+
+        nativeBuildInputs = [
+          pkg-config
+        ];
+
+        buildInputs =
+          [
+            openssl
+            sqlite
+          ]
+          ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Security
+          ];
+
+        env.LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
+      };
+
+    default =
+      with pkgs;
+      mkShell {
+        name = "nix-index";
+
+        inputsFrom = [ devShells.minimal ];
+
+        nativeBuildInputs = [
+          rustc
+          cargo
+          clippy
+          rustfmt
+        ];
+
+        env = {
+          LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      };
+
+  };
+  app-shell =
+    with pkgs;
+    mkShellNoCC {
+      name = "nix-index";
+      packages = [ packages.default ];
+    };
+}
--- flake.nix
+++ flake.nix
@@ -3,103 +3,44 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-compat }:
-    let
-      inherit (nixpkgs) lib;
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
-      forAllSystems = lib.genAttrs systems;
-      nixpkgsFor = nixpkgs.legacyPackages;
-    in
+  outputs =
     {
-      packages = forAllSystems (system: {
-        default = with nixpkgsFor.${system}; rustPlatform.buildRustPackage {
-          pname = "nix-index";
-          inherit ((lib.importTOML ./Cargo.toml).package) version;
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        inherit (nixpkgs) lib;
+        classic = import ./. { inherit system inputs; };
+      in
+      {
 
-          src = lib.sourceByRegex self [
-            "(examples|src)(/.*)?"
-            ''Cargo\.(toml|lock)''
-            ''command-not-found\.sh''
-          ];
+        inherit (classic) packages devShells;
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+        apps = {
+          nix-index = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/nix-index";
           };
-
-          nativeBuildInputs = [ pkg-config ];
-          buildInputs = [ openssl curl sqlite ]
-            ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
-
-          postInstall = ''
-            substituteInPlace command-not-found.sh \
-              --subst-var out
-            install -Dm555 command-not-found.sh -t $out/etc/profile.d
-          '';
-
-          meta = with lib; {
-            description = "A files database for nixpkgs";
-            homepage = "https://github.com/nix-community/nix-index";
-            license = with licenses; [ bsd3 ];
-            maintainers = [ maintainers.bennofs ];
+          nix-locate = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/nix-locate";
           };
+          default = self.apps.${system}.nix-locate;
         };
-      });
 
-      checks = forAllSystems (system:
+        checks =
           let
             packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self.packages.${system};
             devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self.devShells.${system};
-          in packages // devShells
-      );
-
-      devShells = forAllSystems (system: {
-        minimal = with nixpkgsFor.${system}; mkShell {
-          name = "nix-index";
-
-          nativeBuildInputs = [
-            pkg-config
-          ];
-
-          buildInputs = [
-            openssl
-            sqlite
-          ] ++ lib.optionals stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.Security
-          ];
-
-          env.LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
-        };
-
-        default = with nixpkgsFor.${system}; mkShell {
-          name = "nix-index";
-
-          inputsFrom = [ self.devShells.${system}.minimal ];
-
-          nativeBuildInputs = [ rustc cargo clippy rustfmt ];
-
-          env = {
-            LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
-        };
-      });
-
-      apps = forAllSystems (system: {
-        nix-index = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/nix-index";
-        };
-        nix-locate = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/nix-locate";
-        };
-        default = self.apps.${system}.nix-locate;
-      });
-    };
+          in
+          packages // devShells;
+      }
+    );
 }
